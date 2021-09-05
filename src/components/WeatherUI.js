8fccc935aa6dfe256489ac44df06027f491d4cc7
mohammadizanloo55 <mohammadizanloo55@gmail.com>
2021-09-05 12:18:03 +0430

    Loading Paragraph Removed

--- src/components/WeatherUI.js
+++ src/components/WeatherUI.js
@@ -14,9 +14,6 @@ const WeatherUi = ({data,Loading}) => {
             >
               <div class="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"></div>
               <h2 className="text-center text-white text-xl font-semibold">Loading...</h2>
-              <p className="w-1/3 text-center text-white">
-                This may take a few seconds, please don't close this page.
-              </p>
             </div>
         );
     }
